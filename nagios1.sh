fd640a79ea0a48cfd71e62986eaca10d70086530
Jos Terlage <Jos211@live.nl>
2018-06-14 14:14:15 +0200

    sh file version1

---
+++ nagios1.sh
@@ -0,0 +1,20 @@
+sudo useradd nagios
+sudo apt-get update
+sudo apt-get install build-essential libgd2-xpm-dev openssl libssl-dev unzip
+cd
+curl -L -O http://nagios-plugins.org/download/nagios-plugins-2.2.1.tar.gz
+tar zxf nagios-plugins-*.tar.gz
+cd nagios-plugins-*
+./configure --with-nagios-user=nagios --with-nagios-group=nagios --with-openssl
+sudo make
+sudo make install
+cd
+curl -L -O https://github.com/NagiosEnterprises/nrpe/releases/download/nrpe-3.2.1/nrpe-3.2.1.tar.gz
+tar zxf nrpe-*.tar.gz
+cd nrpe-*
+./configure --enable-command-args --with-nagios-user=nagios --with-nagios-group=nagios --with-ssl=/usr/bin/openssl --with-ssl-lib=/usr/lib/x86_64-linux-gnu
+make all
+sudo make install
+sudo make install-config
+sudo make install-init
+sudo sed -i -e 's/allowed_hosts=127.0.0.1,::1/allowed_hosts=127.0.0.1,::1,10.0.0.6/g' /usr/local/nagios/etc/nrpe.cfg
